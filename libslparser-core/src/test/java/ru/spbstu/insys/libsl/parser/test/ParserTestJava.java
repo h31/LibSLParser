505a6a0752c8b617c28ff602db9484af5d1a77bf
Artyom Aleksyuk <artyom.aleksyuk@gmail.com>
2021-02-01 22:04:37 +0300

    Add Java test

---
+++ libslparser-core/src/test/java/ru/spbstu/insys/libsl/parser/test/ParserTestJava.java
@@ -0,0 +1,38 @@
+package ru.spbstu.insys.libsl.parser.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.spbstu.insys.libsl.parser.LibraryDecl;
+import ru.spbstu.insys.libsl.parser.ModelParser;
+import ru.spbstu.insys.libsl.parser.PrettyprinterKt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParserTestJava {
+    private InputStream readResourceAsString(String name) {
+        return ParserTest.class.getResourceAsStream(name);
+    }
+
+    @Test
+    public void z3test() throws IOException {
+        LibraryDecl parsedModel;
+        try (InputStream sourceModel = readResourceAsString("/prettyprinter/Z3.lsl")) {
+            assertNotNull(sourceModel);
+            parsedModel = new ModelParser().parse(sourceModel);
+        }
+        String text = PrettyprinterKt.print(parsedModel);
+        assertEquals("library Z3;", text.substring(0, text.indexOf('\n')));
+        Assertions.assertTrue(parsedModel.getFunctions().stream().anyMatch(func -> func.getName().equals("Z3_mk_bool_sort")));
+        InputStreamReader modelStreamReader = new InputStreamReader(readResourceAsString("/prettyprinter/Z3.lsl"));
+        try (BufferedReader reader = new BufferedReader(modelStreamReader)) {
+            String expected = reader.lines().collect(Collectors.joining("\n", "", "\n"));
+            assertEquals(expected, text);
+        }
+    }
+}
--- libslparser-core/src/test/kotlin/ru/spbstu/insys/libsl/parser/test/ParserTest.kt
+++ libslparser-core/src/test/kotlin/ru/spbstu/insys/libsl/parser/test/ParserTest.kt
@@ -1,8 +1,11 @@
-package ru.spbstu.insys.libsl.parser
+package ru.spbstu.insys.libsl.parser.test
 
 import org.junit.jupiter.api.Test
+import ru.spbstu.insys.libsl.parser.ModelParser
+import ru.spbstu.insys.libsl.parser.print
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 class ParserTest {
     private fun readResourceAsString(name: String) = this.javaClass.classLoader.getResourceAsStream(name)
@@ -15,9 +18,10 @@ class ParserTest {
     @Test
     fun z3test() {
         val sourceModel = assertNotNull(readResourceAsString("prettyprinter/Z3.lsl"))
-        val parsedModel = ModelParser().parse(sourceModel)
+        val parsedModel = sourceModel.use { ModelParser().parse(it) }
         val text = parsedModel.print()
         assertEquals("library Z3;", text.lineSequence().first())
+        assertTrue(parsedModel.functions.any { it.name == "Z3_mk_bool_sort" })
         val expected = assertNotNull(readResourceAsString("prettyprinter/Z3.lsl"))
             .bufferedReader()
             .use { it.readText() }
